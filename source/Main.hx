0e080ec2a3186cf27d2f408806e3e0f89385e6f5
johnny <107599365+JonnycatMeow@users.noreply.github.com>
2022-11-19 21:58:12 -0800

    Update Main.hx

--- source/Main.hx
+++ source/Main.hx
@@ -95,8 +95,14 @@ class Main extends Sprite
 
 		addChild(new FlxGame(gameWidth, gameHeight, initialState, zoom, framerate, framerate, skipSplash, startFullscreen));
 
-		fpsVar = new FPS(10, 3, 0xFFFFFF);
-		addChild(fpsVar);
-		fpsVar.visible = true;
+#if !mobile
+ fpsVar = new FPS(10, 3, 0xFFFFFF);
+ addChild(fpsVar);
+ Lib.current.stage.align = "tl";
+ Lib.current.stage.scaleMode = StageScaleMode.NO_SCALE;
+ if(fpsVar != null) {
+ 	fpsVar.visible = ClientPrefs.showFPS;
+ }
+ #end
 	}
 }
